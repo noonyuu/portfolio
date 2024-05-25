287af2eebc0ff6865242bc4442455613f3f0abb6
shimizu <noon3m.op.sh@gmail.com>
2024-05-26 04:06:22 +0900

    [add]フッターの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Layout from './pages/Layout'
 import Header from './features/Header/Header'
+import Footer from './features/Footer/footer'
 
 function App() {
   return (
     <div className="mx-auto">
       <Header />
       <Layout />
+      <Footer />
     </div>
   )
 }
