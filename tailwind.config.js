f3ed574795f1af29420783fbea94fa2783795d59
shimizu <noon3m.op.sh@gmail.com>
2024-08-24 18:45:44 +0900

    [add]自己紹介カードコンポーネント作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       colors: {
         'main-color': '#FAF0D7',
         'sub-color': '#EEEDEB'
+      },
+      boxShadow: {
+        custom: '5px 4px 4px 5px rgba(185, 185, 185, 0.25)'
       }
     }
   },
