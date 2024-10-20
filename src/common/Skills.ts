c42261d505f5d5d6edf74a2a65ad497e879c39dc
shimizu <noon3m.op.sh@gmail.com>
2024-10-21 00:50:08 +0900

    [add] 言語iconの追加と作品の追加

--- src/common/Skills.ts
+++ src/common/Skills.ts
@@ -7,6 +7,11 @@ import Python from '@/assets/skill-icon/python.svg'
 import tailwindcss from '@/assets/skill-icon/tailwindcss.svg'
 import TypeScript from '@/assets/skill-icon/typescript.svg'
 import OpenAI from '@/assets/skill-icon/openai.svg'
+import cPlus from '@/assets/skill-icon/cPlus.svg'
+import firebase from '@/assets/skill-icon/firebase.svg'
+import Flutter from '@/assets/skill-icon/flutter.svg'
+import next from '@/assets/skill-icon/next.svg'
+import swift from '@/assets/skill-icon/swift.svg'
 
 export const SKILLS = [
   {
@@ -44,5 +49,25 @@ export const SKILLS = [
   {
     icon: OpenAI,
     name: 'OpenAI'
+  },
+  {
+    icon: cPlus,
+    name: 'c++'
+  },
+  {
+    icon: firebase,
+    name: 'firebase'
+  },
+  {
+    icon: Flutter,
+    name: 'Flutter'
+  },
+  {
+    icon: next,
+    name: 'Next.js'
+  },
+  {
+    icon: swift,
+    name: 'Swift'
   }
 ]
