0b3763e5bd5eba9261f20c31ef3decf953a3355c
shimizu <noon3m.op.sh@gmail.com>
2024-10-21 00:31:17 +0900

    [add] @types/node and path dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': '/src',
-    }
+  server: {
+    open: true
   },
-  plugins: [react()]
+  plugins: [react(), tsconfigPaths()]
 })
