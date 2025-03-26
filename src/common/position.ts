6968c0f7c492eaa48d133571029f257de937f27e
shimizu <noon3m.op.sh@gmail.com>
2025-03-26 21:14:23 +0900

    関数の統一

--- src/common/position.ts
+++ src/common/position.ts
@@ -1,6 +1,6 @@
 import { RefObject, useCallback } from 'react'
 
-// 引数のtargetProperty をDOMRectのもつPropertyに限定する
+// 引数のtargetPropertyをDOMRectのもつPropertyに限定する
 type DOMRectProperty = 'height' | 'width' | 'x' | 'y' | 'top' | 'right' | 'bottom' | 'left'
 
 export const Position = <T extends HTMLElement>(elementRef: RefObject<T>) => {
@@ -12,7 +12,6 @@ export const Position = <T extends HTMLElement>(elementRef: RefObject<T>) => {
       }
 
       // clientRectがundefinedのときはデフォルトで0を返すようにする
-      console.warn('Element reference is null or undefined. Returning default value of 0.')
       return 0
     },
     [elementRef]
@@ -20,25 +19,5 @@ export const Position = <T extends HTMLElement>(elementRef: RefObject<T>) => {
 
   return {
     getPosition
-  }
-}
-
-export const PositionCard = <T extends HTMLElement>(elementRef: RefObject<T>) => {
-  const getPositionCard = useCallback(
-    (targetProperty: DOMRectProperty): number => {
-      const clientRect = elementRef.current?.getBoundingClientRect()
-      if (clientRect) {
-        return clientRect[targetProperty]
-      }
-
-      // clientRectがundefinedのときはデフォルトで0を返すようにする
-      console.warn('Element reference is null or undefined. Returning default value of 0.')
-      return 0
-    },
-    [elementRef]
-  )
-
-  return {
-    getPositionCard
   }
 }
